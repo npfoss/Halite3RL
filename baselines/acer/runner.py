d3adf32dd4d6928e34710cae56e89107cfb4f7da
Nate Foss <npfoss@gmail.com>
2018-11-30 12:46:05 -0500

    comment out old version

--- baselines/acer/runner.py
+++ baselines/acer/runner.py
@@ -49,42 +49,42 @@ class HaliteRunner:
         mb_obs = enc_obs_to_obs(enc_obs)
 
 
-        # enc_obs = np.split(self.obs, self.nstack, axis=3)  # so now list of obs steps
-        enc_obs = np.split(self.env.stackedobs, self.env.nstack, axis=-1)
-        mb_obs, mb_actions, mb_mus, mb_dones, mb_rewards = [], [], [], [], []
-        for _ in range(self.nsteps):
-            actions, mus, states = self.model._step(self.obs, S=self.states, M=self.dones)
-            mb_obs.append(np.copy(self.obs))
-            mb_actions.append(actions)
-            mb_mus.append(mus)
-            mb_dones.append(self.dones)
-            obs, rewards, dones, _ = self.env.step(actions)
-            # states information for statefull models like LSTM
-            self.states = states
-            self.dones = dones
-            self.obs = obs
-            mb_rewards.append(rewards)
-            enc_obs.append(obs[..., -self.nc:])
-        mb_obs.append(np.copy(self.obs))
-        mb_dones.append(self.dones)
+        # # enc_obs = np.split(self.obs, self.nstack, axis=3)  # so now list of obs steps
+        # enc_obs = np.split(self.env.stackedobs, self.env.nstack, axis=-1)
+        # mb_obs, mb_actions, mb_mus, mb_dones, mb_rewards = [], [], [], [], []
+        # for _ in range(self.nsteps):
+        #     actions, mus, states = self.model._step(self.obs, S=self.states, M=self.dones)
+        #     mb_obs.append(np.copy(self.obs))
+        #     mb_actions.append(actions)
+        #     mb_mus.append(mus)
+        #     mb_dones.append(self.dones)
+        #     obs, rewards, dones, _ = self.env.step(actions)
+        #     # states information for statefull models like LSTM
+        #     self.states = states
+        #     self.dones = dones
+        #     self.obs = obs
+        #     mb_rewards.append(rewards)
+        #     enc_obs.append(obs[..., -self.nc:])
+        # mb_obs.append(np.copy(self.obs))
+        # mb_dones.append(self.dones)
 
-        enc_obs = np.asarray(enc_obs, dtype=self.obs_dtype).swapaxes(1, 0)
-        mb_obs = np.asarray(mb_obs, dtype=self.obs_dtype).swapaxes(1, 0)
-        mb_actions = np.asarray(mb_actions, dtype=self.ac_dtype).swapaxes(1, 0)
-        mb_rewards = np.asarray(mb_rewards, dtype=np.float32).swapaxes(1, 0)
-        mb_mus = np.asarray(mb_mus, dtype=np.float32).swapaxes(1, 0)
+        # enc_obs = np.asarray(enc_obs, dtype=self.obs_dtype).swapaxes(1, 0)
+        # mb_obs = np.asarray(mb_obs, dtype=self.obs_dtype).swapaxes(1, 0)
+        # mb_actions = np.asarray(mb_actions, dtype=self.ac_dtype).swapaxes(1, 0)
+        # mb_rewards = np.asarray(mb_rewards, dtype=np.float32).swapaxes(1, 0)
+        # mb_mus = np.asarray(mb_mus, dtype=np.float32).swapaxes(1, 0)
 
-        mb_dones = np.asarray(mb_dones, dtype=np.bool).swapaxes(1, 0)
+        # mb_dones = np.asarray(mb_dones, dtype=np.bool).swapaxes(1, 0)
 
-        # done is True on the last frame of a game
-        # (THIS IS THE IMPORTANT ONE TO GET RIGHT)
-        # mask is True on the first frame of a game (generated from dones)
+        # # done is True on the last frame of a game
+        # # (THIS IS THE IMPORTANT ONE TO GET RIGHT)
+        # # mask is True on the first frame of a game (generated from dones)
         
-        mb_masks = mb_dones # Used for statefull models like LSTM's to mask state when done
-        mb_dones = mb_dones[:, 1:] # Used for calculating returns. The dones array is now aligned with rewards
+        # mb_masks = mb_dones # Used for statefull models like LSTM's to mask state when done
+        # mb_dones = mb_dones[:, 1:] # Used for calculating returns. The dones array is now aligned with rewards
 
-        # shapes are now [nenv, nsteps, []]
-        # When pulling from buffer, arrays will now be reshaped in place, preventing a deep copy.
+        # # shapes are now [nenv, nsteps, []]
+        # # When pulling from buffer, arrays will now be reshaped in place, preventing a deep copy.
 
         return enc_obs, mb_obs, mb_actions, mb_rewards, mb_mus, mb_dones, mb_masks
 
