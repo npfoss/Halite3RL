947ac010ac35b69571aa83f40888f34fcfeda938
Kent Slaney <kent@slaney.org>
2018-12-04 10:45:31 -0500

    made dependencies installation part of zipping process

--- zipper.sh
+++ zipper.sh
@@ -1,2 +1,15 @@
 rm -f halite.zip
 zip -r halite.zip . -x@exclude.lst
+
+if [ ! -d dependencies ]; then
+  mkdir dependencies;
+fi
+if [ ! -d dependencies/baselines/acer ]; then
+  mkdir -p dependencies/baselines/acer;
+fi
+cd dependencies
+
+pip install --no-dependencies --target . dill gym joblib
+pip install --no-dependencies --target baselines/acer dill
+
+zip -r ../halite.zip *
