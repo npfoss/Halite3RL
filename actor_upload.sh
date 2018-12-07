c674bff14ba4c8116558591db6e22117da549767
Kent Slaney <kent@slaney.org>
2018-12-07 03:31:18 -0500

    fixed replay location

--- actor_upload.sh
+++ actor_upload.sh
@@ -1,6 +1,7 @@
-cd sync
-git add .
-git commit -m `python3 -c "import time; print(int(time.time()*1e9))"`
-git pull --no-edit && git pull --no-edit && git pull --no-edit
-git push
-cp actor.ckpt ..
+param () { python3 -c "import json; print(json.load(open('$1'))['$2'] + 1)"; }
+url () { python3 -c "import json; print((lambda x: x['base_url']+'/$2?secret='+x['secret'])(json.load(open('$1'))))"; }
+curl -OJ `url params.json download/weights`
+cd sync/replays
+ls *.phlt | while read line; do
+	curl -X POST -F "file=@$line" `url ../../params.json upload/replay`
+done
