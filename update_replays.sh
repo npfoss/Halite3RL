bff0fd8caaa19a5177f23057aaf14ccae4bba437
Kent Slaney <kent@slaney.org>
2018-12-07 04:09:55 -0500

    upload weights method

--- update_replays.sh
+++ update_replays.sh
@@ -1,6 +1,6 @@
 param () { python3 -c "import json; print(json.load(open('$1'))['$2'] + 1)"; }
 url () { python3 -c "import json; print((lambda x: x['base_url']+'/$2?secret='+x['secret'])(json.load(open('$1'))))"; }
-curl -F @weights=actor.ckpt `url params.json upload/weights`
+curl -X POST -F @weights=actor.ckpt `url params.json upload/weights`
 cd replays
 curl --silent `url ../params.json list/replays` | while read line; do
 	curl --silent -OJ `url ../params.json "download/replay/$line"`
