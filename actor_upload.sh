e0dcde9ead1cb5bf0da890eb28d89ed26814c1f7
Kent Slaney <kent@slaney.org>
2018-12-07 04:14:31 -0500

    fixed actor upload

--- actor_upload.sh
+++ actor_upload.sh
@@ -1,7 +1,8 @@
 param () { python3 -c "import json; print(json.load(open('$1'))['$2'] + 1)"; }
 url () { python3 -c "import json; print((lambda x: x['base_url']+'/$2?secret='+x['secret'])(json.load(open('$1'))))"; }
-curl -OJ `url params.json download/weights`
+curl `url params.json download/weights` > actor.ckpt
 cd sync/replays
 ls *.phlt | while read line; do
 	curl -X POST -F "file=@$line" `url ../../params.json upload/replay`
+	rm $line
 done
