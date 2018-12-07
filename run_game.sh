97adaf95753f0fd2e02435a3c7ec80d07eeb32b8
Nate Foss <npfoss@gmail.com>
2018-12-07 16:04:07 -0500

    no timeout

--- run_game.sh
+++ run_game.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./halite --replay-directory replays/ -vvv --width 32 --height 32 "python3 MyBot.py --learning=true" "python3 MyBot.py --learning=true"
+./halite --replay-directory replays/ --no-timeout -vvv --width 32 --height 32 "python3 MyBot.py --learning=true" "python3 MyBot.py --learning=true"
