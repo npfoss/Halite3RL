0352541e41aaf17898b95593ccb45f71ae511b75
Kent Slaney <kent@slaney.org>
2018-12-06 22:46:36 -0500

    added pull frequency param

--- update_replays.sh
+++ update_replays.sh
@@ -1,8 +1,9 @@
-cd sync
-git pull
-ls -1 replays/*.phlt | tail -n +`python3 -c 'import json; print(json.load(open("../params.json"))["disk_buffer_size"] + 1)'` | xargs rm
-cp ../actor.ckpt .
-git add .
-git commit -m `python3 -c "import time; print(int(time.time()*1e9))"`
-git push
-
+if ((`date +"%s"` - `git log -1 --format=%ct` > `python3 -c 'import json; print(json.load(open("../params.json"))["pull_frequency"] + 1)'`)); then
+    cd sync
+    git pull
+    ls -1 replays/*.phlt | tail -n +`python3 -c 'import json; print(json.load(open("../params.json"))["disk_buffer_size"] + 1)'` | xargs rm
+    cp ../actor.ckpt .
+    git add .
+    git commit -m `python3 -c "import time; print(int(time.time()*1e9))"`
+    git push
+fi
