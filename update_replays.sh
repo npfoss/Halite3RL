7146e48127b3314b03ebede4e7e49f28677a63d2
Kent Slaney <kent@slaney.org>
2018-12-07 02:44:25 -0500

    fixed server list

--- update_replays.sh
+++ update_replays.sh
@@ -1,10 +1,10 @@
-param () { python3 -c "import json; print(json.load(open('$1'))['$2'] + 1)" }
+param () { python3 -c "import json; print(json.load(open('$1'))['$2'] + 1)"; }
 url () { python3 -c "import json; print((lambda x: x['base_url']+'/$2?secret='+x['secret'])(json.load(open('$1'))))"; }
 if [ `python -c 'import os, json, time; print(int(time.time() - os.stat(".git/FETCH_HEAD").st_mtime > json.load(open("params.json"))["pull_frequency"]))'` ]; then
 	curl -F @weights=actor.ckpt `url params.json upload/weights`
 	cd replays
-	curl `url ../params.json list/replays` | while read line; do
-		curl -OJ `url ../params.json "download/replay/$line"`
+	curl --silent `url ../params.json list/replays` | while read line; do
+		curl --silent -OJ `url ../params.json "download/replay/$line"`
 	done
-	ls -1 *.phlt | tail -n +`url ../params.json disk_buffer_size` | xargs rm
+	ls -1 *.phlt | tail -n +`param ../params.json disk_buffer_size` | xargs rm
 fi
