8f7821e05b360c476e5af977fd269d71983956dd
RLaDOS <npfoss@fast-learner.us-central1-f.c.halite3rl.internal>
2018-12-09 19:19:47 +0000

    merge

---
+++ insert_expert_files.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+rm -rf expert_replays
+mkdir expert_replays
+
+python3 -m hlt_client replay user -i 2807 -l 100 -d expert_replays
+# 2807 = teccles
+# -l = num games
+# -d = target directory
+
+
+python3 hlt_to_phlt.py
+./actor_upload.sh 
+
+rm -rf expert_replays
+
