284da3bc12226005bb0599762cdc136224c6fae6
Nate Foss <npfoss@gmail.com>
2018-12-09 18:29:02 -0500

    actually fixed the bug this time

--- update_replays.sh
+++ update_replays.sh
@@ -2,7 +2,7 @@ param () { python3 -c "import json; print(json.load(open('$1'))['$2'] + 1)"; }
 url () { python3 -c "import json; print((lambda x: x['base_url']+'/$2?secret='+x['secret'])(json.load(open('$1'))))"; }
 curl -X POST -F @weights=actor.ckpt `url params.json upload/weights`
 cd replays
+ls -1r *.phlt | tail -n +`param ../params.json disk_buffer_size` | xargs rm
 curl --silent `url ../params.json list/replays` | while read line; do
 	curl --silent -OJ `url ../params.json "download/replay/$line"`
 done
-ls -1r *.phlt | tail -n +`param ../params.json disk_buffer_size` | xargs rm
