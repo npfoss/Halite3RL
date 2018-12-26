89228fb1d9091fb5b6d927a9e6353e1fc31a6ad1
Nate Foss <npfoss@gmail.com>
2018-12-26 16:33:11 -0500

    decrease first filter size

--- baselines/common/models.py
+++ baselines/common/models.py
@@ -32,7 +32,7 @@ def halite_cnn(unscaled_images, **conv_kwargs):
     """
     scaled_images = tf.cast(unscaled_images, tf.float32) / 255.
     activ = tf.nn.relu
-    h = activ(conv(scaled_images, 'c1', nf=32, rf=8, stride=2, init_scale=np.sqrt(2),
+    h = activ(conv(scaled_images, 'c1', nf=32, rf=6, stride=2, init_scale=np.sqrt(2),
                    **conv_kwargs))
     h2 = activ(conv(h, 'c2', nf=64, rf=4, stride=2, init_scale=np.sqrt(2), **conv_kwargs))
     h3 = activ(conv(h2, 'c3', nf=64, rf=3, stride=1, init_scale=np.sqrt(2), **conv_kwargs))
