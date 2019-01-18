e63af271aa23b9992b087f291ad12b38eb5bbcc2
Kent Slaney <kent@slaney.org>
2019-01-18 00:25:09 -0500

    spaces suck

--- replay_parser.py
+++ replay_parser.py
@@ -269,9 +269,9 @@ def read_mus(player_id):
             if "mu:" in line:
                 mu_str = line.split("mu:")[-1]
                 mus.append(json.loads(mu_str))
-			if "rth:" in line:
+            if "rth:" in line:
                 mu_str = line.split("rth:")[-1]
-				returned_to_home.append(json.loads(rth_str))
+                returned_to_home.append(json.loads(rth_str))
     return mus, returned_to_home
 
 def fake_mus(player_id, data):
