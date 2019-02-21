49bd592d9e04be6d74ee4bf92f17d17a0300dd88
Nate Foss <npfoss@gmail.com>
2019-02-21 16:50:56 -0500

    make sure no weird concurrent actor stuff happens

--- actor.py
+++ actor.py
@@ -10,10 +10,10 @@ current_proc = None
 while True:
     for i in range(runs):
         runner.run()
+    current_proc = subprocess.Popen(['sh', 'actor_upload.sh'])
     while current_proc is not None and current_proc.poll() is None:
         # not done yet!
         print("waiting on previous upload")
         # wait n seconds
         time.sleep(1)
-    current_proc = subprocess.Popen(['sh', 'actor_upload.sh'])
 
